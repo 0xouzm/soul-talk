b1962df7b020f4edbfe19c2d3254083ff8749ecb
wujj <wujj@cpchain.io>
2019-08-26 14:18:18 +0800

    chapter 4

--- src/soul_talk/core.clj
+++ src/soul_talk/core.clj
@@ -1,9 +1,10 @@
 (ns soul-talk.core
   (:require
-    ;[ring.adapter.jetty :as jetty]
-    ;[ring.util.response :as response]
+    [ring.adapter.jetty :as jetty]
+    [ring.util.http-response :as resp]
+    [ring.util.response :refer [redirect]]
     [ring.middleware.reload :refer [wrap-reload]]
-    [compojure.core :refer [routes GET defroutes]]
+    [compojure.core :refer [routes GET POST defroutes]]
     ;[clojure.java.io :as io]
     [compojure.route :as route]
     [selmer.parser :as parser]
@@ -14,13 +15,25 @@
 
 
 (defn home-handle [request]
-  (parser/render-file "index.html"
-                      {:ip (:remote-addr request)}))
+  (parser/render-file "index.html" request))
+
+(defn handle-logout [request]
+  (do
+    (assoc request :session {})
+    (redirect "/")))
 
 (defn login-page [request]
   "this is login page"
-   (parser/render-file "login.html" {})
+  (parser/render-file "login.html" request)
   )
+
+(defn handle-login [email password request]
+  (if (and (= email "wujj@gmail.com")
+           (= password "1"))
+    (home-handle (assoc-in request [:session :identity] email))
+    (login-page (assoc request :error "用户名密码不对"))))
+
+
 
 (defn error-page [error-details]
   {:status  (:status error-details)
@@ -31,6 +44,8 @@
   app-routes
   (GET "/" request home-handle)
   (GET "/login" request (login-page request))
+  (POST "/login" [email password :as request] (handle-login email password request))
+  (GET "/logout" request (handle-logout request))
   (GET "/about" [] (str "这是关于我的页面"))
   (route/not-found error-page))
 
@@ -45,8 +60,8 @@
   (-> app-routes
       (wrap-nocache)
       (wrap-reload)
-      (wrap-webjars)  ;; 这行添加的
-      (wrap-defaults site-defaults)
+      (wrap-webjars)                                        ;; 这行添加的
+      (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
       )
   )
 
